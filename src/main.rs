054b036d803ff252b2543d2f891d9ab2ac34b9ee
Sebastian Elstadt <sebastian@elstadt.com>
2025-12-23 12:30:07 +0200

    added indicatif crate for nice progress ui

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use clap::Parser;
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use regex::Regex;
 use std::{
     io::{BufRead, BufReader},
     process::{Command, Stdio},
-    thread,
+    sync::Arc,
+    thread::{self, JoinHandle},
+    time::Duration,
 };
 
 #[derive(Parser, Debug)]
@@ -22,6 +25,9 @@ fn main() {
         .unwrap();
 
     let hop_line_regex = Regex::new(r"^\s*(?<i>\d+).*\((?<a>[\d\.]+)\)").unwrap();
+    let multiprog = MultiProgress::new();
+    let multiprog = Arc::new(multiprog);
+    let mut thread_handles: Vec<JoinHandle<()>> = vec![];
 
     for line in BufReader::new(traceroute.stdout.take().unwrap()).lines() {
         if let Ok(line) = line {
@@ -32,16 +38,31 @@ fn main() {
             if let Some(parts) = hop_line_regex.captures(&line) {
                 let index: u8 = parts["i"].parse().unwrap();
                 let ip_addr = parts["a"].to_string();
+                let multiprog = multiprog.clone();
 
-                thread::spawn(move || {
-                    analyse_hop(index, &ip_addr);
+                let handle = thread::spawn(move || {
+                    analyse_hop(index, &ip_addr, multiprog);
                 });
+
+                thread_handles.push(handle);
             }
         }
     }
+
+    for handle in thread_handles {
+        handle.join().unwrap();
+    }
 }
 
-fn analyse_hop(index: u8, ip_addr: &str) {
-    // should print: "hopped over {ip_addr}: analysing..."
-    // then line should update to "hopped over {ip_addr}: <the data found>"
+fn analyse_hop(index: u8, ip_addr: &str, multiprog: Arc<MultiProgress>) {
+    let progbar = multiprog.add(ProgressBar::new(1));
+    progbar.set_style(ProgressStyle::with_template("{prefix:>3}: {msg} {spinner}").unwrap());
+    progbar.set_prefix(format!("{}", index));
+    progbar.set_message(format!("{:<15} - analysing...", ip_addr));
+    progbar.enable_steady_tick(Duration::from_millis(100));
+
+    // todo
+    thread::sleep(Duration::from_secs(2));
+
+    progbar.finish_with_message(format!("{:<15} - someserver.net (earth)", ip_addr));
 }
