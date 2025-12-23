0e31c5c44bed9adc24bfa0ae2458fe2455061851
Sebastian Elstadt <sebastian@elstadt.com>
2025-12-23 13:40:09 +0200

    get geodb. wonder if this will work...

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ use dns_lookup::lookup_addr;
 use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use regex::Regex;
 use std::{
-    io::{BufRead, BufReader},
+    fs::{self, File},
+    io::{BufRead, BufReader, BufWriter},
+    path::Path,
     process::{Command, Stdio},
     sync::Arc,
     thread::{self, JoinHandle},
@@ -19,6 +21,9 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
+    let geodb_reader = get_geodb().unwrap();
+
+    println!("mapping internet route to '{}'...", args.target);
     let mut traceroute = Command::new("traceroute")
         .arg(args.target)
         .stdout(Stdio::piped())
@@ -53,6 +58,40 @@ fn main() {
     for handle in thread_handles {
         handle.join().unwrap();
     }
+
+    println!("program has completed.");
+}
+
+fn get_geodb() -> Result<maxminddb::Reader<Vec<u8>>, Box<dyn std::error::Error>> {
+    println!("checking local geo db...");
+
+    if Path::new("res/geo.mmdb").exists() {
+        return maxminddb::Reader::open_readfile("/path/to/GeoLite2-City.mmdb")
+            .map_err(|e| e.into());
+    }
+
+    println!("missing local db. downloading...");
+    if !Path::new("res/").exists() {
+        fs::create_dir("res")?;
+    }
+
+    // got this from https://github.com/P3TERX/GeoLite.mmdb
+    // i assume this is ok
+    let response = ureq::get("https://git.io/GeoLite2-City.mmdb")
+        .call()
+        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+
+    if !response.status().is_success() {
+        return Err(format!("HTTP {}", response.status()).into());
+    }
+
+    let dest = File::create("res/geo.mmdb").unwrap();
+    let mut dest_writer = BufWriter::new(dest);
+    std::io::copy(&mut response.into_body().into_reader(), &mut dest_writer).unwrap();
+    println!("geo db has been downloaded.");
+
+    let reader = maxminddb::Reader::open_readfile("/path/to/GeoLite2-City.mmdb")?;
+    return Ok(reader);
 }
 
 fn analyse_hop(index: u8, ip_addr_str: &str, multiprog: Arc<MultiProgress>) {
@@ -65,9 +104,14 @@ fn analyse_hop(index: u8, ip_addr_str: &str, multiprog: Arc<MultiProgress>) {
     // reverse DNS lookup
     let ip_addr: std::net::IpAddr = ip_addr_str.parse().unwrap();
     let host = lookup_addr(&ip_addr).unwrap_or("unknown".to_string());
-    progbar.set_message(format!("{:<15} - analysing...\nhost: {}\n", ip_addr_str, host));
+    progbar.set_message(format!(
+        "{:<15} - analysing...\nhost: {}\n",
+        ip_addr_str, host
+    ));
 
     thread::sleep(Duration::from_secs(2));
 
     progbar.finish_with_message(format!("{}\nhost: {}\n", ip_addr_str, host));
 }
+
+//https://git.io/GeoLite2-City.mmdb
